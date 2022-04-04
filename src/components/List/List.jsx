fba035c03ec1e236dfd28f4216243772042f0590
Salman <salman9915189734@gmail.com>
2022-04-04 22:02:50 +0530

    List Section Added

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import useStyles from "./styles";
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
 
 const List = () => {
+    const classes = useStyles();
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
+
+    const places = [{ name: 'Cool Place' }, { name: 'Best Beer' }, { name: 'Best Steak' }];
+
     return (
-        <div>
-            <h1> List</h1>
+        <div className={classes.container}>
+            <Typography varient="h4">
+                Restaurants, Hotels & Attractions around you
+            </Typography>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Type</InputLabel>
+                <Select vlaue={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Ratings</InputLabel>
+                <Select vlaue={type} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3.0</MenuItem>
+                    <MenuItem value={4}>Above 4.0</MenuItem>
+                    <MenuItem value={4.5}>Above 4..5</MenuItem>
+                </Select>
+            </FormControl>
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, index) => (
+                    <Grid item key={index} xs={12}>
+                        <PlaceDetails place={place} />
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     )
 }
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const PlaceDetails = () => {
+const PlaceDetails = ({ place }) => {
     return (
         <div>
-            Place Details
+            {place.name}
         </div>
     )
 }
