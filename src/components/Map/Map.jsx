e10d310e57585ae93f0a76400b753fb0a98ccde5
Salman <salman9915189734@gmail.com>
2022-04-04 21:27:09 +0530

    map section added

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width : 600px)');
+    const coordinates = { lat: 30.74, lng: 76.78 };
     return (
-        <div>
-            <h1> Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyBsqk6nUIksib9hbom-HE2niTzDEh9EjGs' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
         </div>
     )
 }
 
-export default Map
+export default Map;
